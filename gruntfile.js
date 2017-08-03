b804fde5e4d596e39c490109766dc32802fb4aa3
edwinchen85 <edwinchen85@gmail.com>
2017-08-03 14:23:35 +0800

    Add "wrapper" function

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,10 @@
 // The "wrapper" function
+module.exports = function(grunt) {
 
-// Project and task configuration
+  // Project and task configuration
 
-// Loading Grunt plugins and tasks
+  // Loading Grunt plugins and tasks
 
-// Custom tasks
+  // Custom tasks
+
+}
