a5ca515887af0d6a7b2059cac76dacf2d1c93311
edwinchen85 <edwinchen85@gmail.com>
2017-08-03 15:06:49 +0800

    Create sass task and set its config

--- gruntfile.js
+++ gruntfile.js
@@ -3,13 +3,31 @@ module.exports = function(grunt) {
 
   // Project and task configuration
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    /**
+     * Grunt Sass
+     * Compile Sass to CSS using node-sass
+     * https://www.npmjs.com/package/grunt-sass
+     */
+    sass: {
+
+      options: {
+        sourceMap: false
+      },
+      dist: {
+        files: {
+          'css/style.css' : 'assets/scss/style.scss'
+        }
+      }
+
+    }
   });
 
   // Loading Grunt plugins and tasks
   require('load-grunt-tasks')(grunt);
 
   // Custom tasks
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['sass']);
 
 }
